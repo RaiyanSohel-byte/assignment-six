fcc3104e3421095810b62afaa5bd1e6d38c07d54
RaiyanSohel-byte <afnan19262@gmail.com>
2025-09-07 00:30:40 +0600

    tree details added

--- scripts/index.js
+++ scripts/index.js
@@ -22,5 +22,38 @@ const displayCategories = (categories) => {
   `;
     categoryContainer.appendChild(div);
   });
+  loadTreeDetails(categories);
+};
+const loadTreeDetails = () => {
+  fetch(`https://openapi.programming-hero.com/api/plants`)
+    .then((res) => res.json())
+    .then((data) => displayTreeDetails(data));
+};
+const displayTreeDetails = (treeDetails) => {
+  console.log(treeDetails);
+  const cardGrid = document.getElementById("card-grid");
+  treeDetails.plants.forEach((plant) => {
+    const div = document.createElement("div");
+    div.className = "card bg-base-100 w-80 shadow-sm mx-auto";
+    div.innerHTML = `      <figure class="px-5 py-5"><img class="rounded-[8px] w-[311px] h-[186px] " src="${plant.image}" alt="" /></figure>
+            <div class="card body px-5 py-5">
+              <h2 class="card title text-[14px] font-semibold">${plant.name}</h2>
+              <p class="text-[12px]">
+               ${plant.description}
+              </p>
+              <div class="flex justify-between items-center">
+                <div class="badge bg-[#dcfce7] text-[#15803D] mt-[10px]">
+                  ${plant.category}
+                </div>
+                <div class="mt-[10px] font-bold"><p>৳${plant.price}</p></div>
+              </div>
+              <div class="card actions mt-[10px]">
+                <button class="btn btn-primary rounded-3xl bg-[#15803D]">
+                  Add To Cart
+                </button>
+              </div>
+            </div>`;
+    cardGrid.appendChild(div);
+  });
 };
 loadCategories();
