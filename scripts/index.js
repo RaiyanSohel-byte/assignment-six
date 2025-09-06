7034bd51aaec1bfcd89d73a385c2512c7387c0b1
RaiyanSohel-byte <afnan19262@gmail.com>
2025-09-07 02:35:33 +0600

    all items showing nothing bug fixed

--- scripts/index.js
+++ scripts/index.js
@@ -24,14 +24,17 @@ const displayCategories = (categories) => {
   `;
     categoryContainer.appendChild(div);
   });
-  loadTreeCards(categories);
+  loadTreeCards();
 
+  document
+    .getElementById("all-items")
+    .classList.add("bg-[#15803d]", "text-white", "rounded-sm");
   categoryContainer.addEventListener("click", (e) => {
     categoryContainer.querySelectorAll(".categories").forEach((category) => {
       category.classList.remove("bg-[#15803d]", "text-white", "rounded-sm");
     });
 
-    if (e.target.localName === "a") {
+    if (e.target.localName === "a" && e.target.id !== "all-items") {
       e.target.classList.add("bg-[#15803d]", "text-white", "rounded-sm");
       //   console.log(e.target.id);
       loadTreeByCategory(e.target.id);
@@ -76,7 +79,34 @@ const displayTreeCards = (treeDetails) => {
 const loadTreeByCategory = (id) => {
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
-    .then((data) => displayTreeCards(data));
+    .then((data) => displayTreeCardByCategory(data));
+};
+const displayTreeCardByCategory = (treeDetails) => {
+  const cardGrid = document.getElementById("card-grid");
+  cardGrid.innerHTML = "";
+  treeDetails.plants.forEach((plant) => {
+    const div = document.createElement("div");
+    div.className = "card bg-base-100 h-[450px] lg:w-80 shadow-sm mx-auto";
+    div.innerHTML = `      <figure class="px-5 py-5"><img class="rounded-[8px] w-[311px] h-[186px] " src="${plant.image}" alt="" /></figure>
+            <div class="card body px-5 py-5">
+              <h2 class="card title text-[14px] font-semibold">${plant.name}</h2>
+              <p class="text-[12px] lg:h-[78px]">
+               ${plant.description}
+              </p>
+              <div class="flex justify-between items-center">
+                <div class="badge bg-[#dcfce7] text-[#15803D] text-xs mt-[10px]">
+                  ${plant.category}
+                </div>
+                <div class="mt-[10px] font-bold"><p>৳${plant.price}</p></div>
+              </div>
+              <div class="card actions mt-[10px]">
+                <button class="btn btn-primary rounded-3xl bg-[#15803D]">
+                  Add To Cart
+                </button>
+              </div>
+            </div>`;
+    cardGrid.appendChild(div);
+  });
 };
 
 loadCategories();
